a59d700e7bc8e306edd4338d82b9ccbaa6c3df72
= <nncl@live.com>
2019-10-20 14:32:42 -0300

    Styling product list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,163 @@
 import React from 'react';
 
+import { ProductList } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+        <ProductList>
+            <li>
+                <img src="http://dummyimage.com/600x600"
+                     alt="Shoe" />
+                <strong>
+                    Freaking awesome show
+                </strong>
+                <span>R$ 129,9</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart
+                            size={16}
+                            color="#fff" />
+                        3
+                    </div>
+                    <span>
+                        Add to Cart
+                    </span>
+                </button>
+            </li>
+            <li>
+                <img src="http://dummyimage.com/600x600"
+                     alt="Shoe" />
+                <strong>
+                    Freaking awesome show
+                </strong>
+                <span>R$ 129,9</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart
+                            size={16}
+                            color="#fff" />
+                        3
+                    </div>
+                    <span>
+                        Add to Cart
+                    </span>
+                </button>
+            </li>
+            <li>
+                <img src="http://dummyimage.com/600x600"
+                     alt="Shoe" />
+                <strong>
+                    Freaking awesome show
+                </strong>
+                <span>R$ 129,9</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart
+                            size={16}
+                            color="#fff" />
+                        3
+                    </div>
+                    <span>
+                        Add to Cart
+                    </span>
+                </button>
+            </li>
+            <li>
+                <img src="http://dummyimage.com/600x600"
+                     alt="Shoe" />
+                <strong>
+                    Freaking awesome show
+                </strong>
+                <span>R$ 129,9</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart
+                            size={16}
+                            color="#fff" />
+                        3
+                    </div>
+                    <span>
+                        Add to Cart
+                    </span>
+                </button>
+            </li>
+            <li>
+                <img src="http://dummyimage.com/600x600"
+                     alt="Shoe" />
+                <strong>
+                    Freaking awesome show
+                </strong>
+                <span>R$ 129,9</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart
+                            size={16}
+                            color="#fff" />
+                        3
+                    </div>
+                    <span>
+                        Add to Cart
+                    </span>
+                </button>
+            </li>
+            <li>
+                <img src="http://dummyimage.com/600x600"
+                     alt="Shoe" />
+                <strong>
+                    Freaking awesome show
+                </strong>
+                <span>R$ 129,9</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart
+                            size={16}
+                            color="#fff" />
+                        3
+                    </div>
+                    <span>
+                        Add to Cart
+                    </span>
+                </button>
+            </li>
+            <li>
+                <img src="http://dummyimage.com/600x600"
+                     alt="Shoe" />
+                <strong>
+                    Freaking awesome show
+                </strong>
+                <span>R$ 129,9</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart
+                            size={16}
+                            color="#fff" />
+                        3
+                    </div>
+                    <span>
+                        Add to Cart
+                    </span>
+                </button>
+            </li>
+            <li>
+                <img src="http://dummyimage.com/600x600"
+                     alt="Shoe" />
+                <strong>
+                    Freaking awesome show
+                </strong>
+                <span>R$ 129,9</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart
+                            size={16}
+                            color="#fff" />
+                        3
+                    </div>
+                    <span>
+                        Add to Cart
+                    </span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }
