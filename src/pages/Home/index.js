24cf3d9ed8b9d2b4e0ceaee7137cdcf6c7373e59
= <nncl@live.com>
2019-10-20 15:12:20 -0300

    Getting data from fake api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,163 +1,56 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import api from '../../services/api';
+import { formatPrice } from '../../utils/format';
 import { ProductList } from './styles';
 import { MdAddShoppingCart } from 'react-icons/md';
 
-export default function Home() {
-    return (
-        <ProductList>
-            <li>
-                <img src="http://dummyimage.com/600x600"
-                     alt="Shoe" />
-                <strong>
-                    Freaking awesome show
-                </strong>
-                <span>R$ 129,9</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart
-                            size={16}
-                            color="#fff" />
-                        3
-                    </div>
-                    <span>
-                        Add to Cart
-                    </span>
-                </button>
-            </li>
-            <li>
-                <img src="http://dummyimage.com/600x600"
-                     alt="Shoe" />
-                <strong>
-                    Freaking awesome show
-                </strong>
-                <span>R$ 129,9</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart
-                            size={16}
-                            color="#fff" />
-                        3
-                    </div>
-                    <span>
-                        Add to Cart
-                    </span>
-                </button>
-            </li>
-            <li>
-                <img src="http://dummyimage.com/600x600"
-                     alt="Shoe" />
-                <strong>
-                    Freaking awesome show
-                </strong>
-                <span>R$ 129,9</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart
-                            size={16}
-                            color="#fff" />
-                        3
-                    </div>
-                    <span>
-                        Add to Cart
-                    </span>
-                </button>
-            </li>
-            <li>
-                <img src="http://dummyimage.com/600x600"
-                     alt="Shoe" />
-                <strong>
-                    Freaking awesome show
-                </strong>
-                <span>R$ 129,9</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart
-                            size={16}
-                            color="#fff" />
-                        3
-                    </div>
-                    <span>
-                        Add to Cart
-                    </span>
-                </button>
-            </li>
-            <li>
-                <img src="http://dummyimage.com/600x600"
-                     alt="Shoe" />
-                <strong>
-                    Freaking awesome show
-                </strong>
-                <span>R$ 129,9</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart
-                            size={16}
-                            color="#fff" />
-                        3
-                    </div>
-                    <span>
-                        Add to Cart
-                    </span>
-                </button>
-            </li>
-            <li>
-                <img src="http://dummyimage.com/600x600"
-                     alt="Shoe" />
-                <strong>
-                    Freaking awesome show
-                </strong>
-                <span>R$ 129,9</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart
-                            size={16}
-                            color="#fff" />
-                        3
-                    </div>
-                    <span>
-                        Add to Cart
-                    </span>
-                </button>
-            </li>
-            <li>
-                <img src="http://dummyimage.com/600x600"
-                     alt="Shoe" />
-                <strong>
-                    Freaking awesome show
-                </strong>
-                <span>R$ 129,9</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart
-                            size={16}
-                            color="#fff" />
-                        3
-                    </div>
-                    <span>
-                        Add to Cart
-                    </span>
-                </button>
-            </li>
-            <li>
-                <img src="http://dummyimage.com/600x600"
-                     alt="Shoe" />
-                <strong>
-                    Freaking awesome show
-                </strong>
-                <span>R$ 129,9</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart
-                            size={16}
-                            color="#fff" />
-                        3
-                    </div>
-                    <span>
-                        Add to Cart
-                    </span>
-                </button>
-            </li>
-        </ProductList>
-    );
+export default class Home extends Component {
+    state = {
+        products: [],
+    };
+
+    async componentDidMount() {
+        const results = await api.get(`products`);
+
+        const data = results.data.map(product => ({
+            ...product,
+            formattedPrice: formatPrice(product.price)
+        }));
+
+        this.setState({ products: data });
+    }
+
+    render() {
+        const { products } = this.state;
+
+        return (
+            <ProductList>
+                {
+                    products.map(product => (
+                        <li key={product.id}>
+                            <img src={product.image}
+                                 alt={product.title} />
+                            <strong>
+                                {product.title}
+                            </strong>
+                            <span>
+                                {product.formattedPrice}
+                            </span>
+                            <button type="button">
+                                <div>
+                                    <MdAddShoppingCart
+                                        size={16}
+                                        color="#fff" />
+                                    3
+                                </div>
+                                <span>
+                                    Add to Cart
+                                </span>
+                            </button>
+                        </li>
+                    ))
+                }
+            </ProductList>
+        );
+    }
 }
