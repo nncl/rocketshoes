8c76637dcb7eceeb08d57cdad410356a84dcb299
= <nncl@live.com>
2019-10-20 20:12:47 -0300

    Separating add to cart action to saga

--- src/store/modules/Cart/sagas.js
+++ src/store/modules/Cart/sagas.js
@@ -1,11 +1,27 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
-import { addToCartSuccess } from './actions';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import { addToCartSuccess, updateCart } from './actions';
 import api from '../../../services/api';
+import { formatPrice } from '../../../utils/format';
 
 function* addToCart({ id }) {
-    const response = yield call(api.get, `/products/${id}`);
+    const productExists = yield select(
+        state => state.cart.find(p => p.id === id)
+    );
 
-    yield put(addToCartSuccess(response.data));
+    if (productExists) {
+        const amount = productExists.amount + 1;
+        yield put(updateCart(id, amount));
+    } else {
+        const response = yield call(api.get, `/products/${id}`);
+
+        const data = {
+            ...response.data,
+            amount: 1,
+            formattedPrice: formatPrice(response.data.price),
+        };
+
+        yield put(addToCartSuccess(data));
+    }
 }
 
 export default all([
